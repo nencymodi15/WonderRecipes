f418068d896671b05679474c9d1176fbe1eda3de
nencymodi15 <80312842+nencymodi15@users.noreply.github.com>
2023-10-23 17:47:40 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WonderRecipes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
