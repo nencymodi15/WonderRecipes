85e2870172e0cf1ceb3b9a8111f6162f09b35d2b
Jigar Mehta <58518925+JigarMehta277@users.noreply.github.com>
2023-10-23 22:05:10 -0400

    ControllerJigar

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WonderRecipes.Models;
+
+namespace WonderRecipes.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = new List<Category>
+        {
+            new Category
+            {
+                CategoryID = 1,
+                CategoryName = "Category 1",
+                ParentCategoryID = null
+            },
+            new Category
+            {
+                CategoryID = 2,
+                CategoryName = "Category 2",
+                ParentCategoryID = null
+            },
+            new Category
+            {
+                CategoryID = 3,
+                CategoryName = "Subcategory 1",
+                ParentCategoryID = 1
+            },
+            new Category
+            {
+                CategoryID = 4,
+                CategoryName = "Subcategory 2",
+                ParentCategoryID = 1
+            },
+        };
+            return View(categories);
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WonderRecipes.Models;
+
+namespace WonderRecipes.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            var users = new List<User>
+        {
+            new User
+            {
+                UserID = 1,
+                Username = "user1",
+                Email = "user1@example.com",
+                PasswordHash = "hashed_password_1",
+                RegistrationDate = DateTime.Now,
+                ProfilePictureBlob = new byte[] { 1, 2, 3, 4, 5 }
+            },
+            new User
+            {
+                UserID = 2,
+                Username = "user2",
+                Email = "user2@example.com",
+                PasswordHash = "hashed_password_2",
+                RegistrationDate = DateTime.Now,
+                ProfilePictureBlob = new byte[] { 6, 7, 8, 9, 10 }
+            }
+        };
+
+            return View(users);
+        }
+    }
+}
